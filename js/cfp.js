e2ac815a176fd9dd21811bb5178791e8055613b9
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-11-29 19:19:36 +0000

    Saving/restoring user values

--- js/cfp.js
+++ js/cfp.js
@@ -1,15 +1,34 @@
 (function(d, w){
 var mailtoForm = new mailto(document.getElementById('mailto-form'));
 var formId = mailtoForm.form.getAttribute('data-spreadsheet-id');
+var fields = ['entry.1910784286', 'entry.174408459'];
+
+
 
 mailtoForm.onSubmit = function(m){
+  var data = m.getData();
   console.log(m);
+
+  w.location = m.getMailtoUrl();
+
+  if ('localStorage' in w) {
+    fields.forEach(function(f){
+      localStorage[f] = data[f];
+    });
+  }
 
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://docs.google.com/forms/d/'+formId+'/formResponse', true);
   xhr.responseType = 'json';
+  xhr.onload = function(){};
   xhr.send(m.getData());
 };
+
+fields.forEach(function(f){
+  if (localStorage[f]) {
+    d.querySelector('[name="'+f+'"]').value = localStorage[f];
+  }
+});
 
 w['Cfp'] = mailtoForm;
 })(document, window);
