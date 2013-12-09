7a176707ad01f18e5c9c1b88940cc4a93912ad96
oncletom <thomas@oncle-tom.net>
2013-12-09 00:01:29 +0000

    POSTING to Gdocs as too long URL break the browser

--- js/cfp.js
+++ js/cfp.js
@@ -1,8 +1,6 @@
 (function(d, w){
-var mailtoForm = new mailto(document.getElementById('mailto-form'));
-var formId = mailtoForm.form.getAttribute('data-spreadsheet-id');
+var mailtoForm = new mailto(document.getElementById('mailto-form'), { preventDefault: false });
 var fields = ['entry.1910784286', 'entry.174408459'];
-var mailBody = mailto.textContent(document.getElementById('mailto-body'));
 
 function saveFields(fields, data){
   if ('localStorage' in w) {
@@ -29,30 +27,9 @@ mailtoForm.formatter = function bracketFormatter(m){
 };
 
 mailtoForm.onSubmit = function(m){
-  var formData = m.getFormData();
   var data = m.getData();
-  var queryString = formData.map(function(f){
-    return encodeURI(f.name) + '=' + encodeURI(f.value);
-  }).join('&');
 
   saveFields(fields, data);
-
-  var el = document.createElement('iframe');
-  el.hidden = true;
-  el.sandbox = '';
-  el.src = 'https://docs.google.com/forms/d/'+formId+'/formResponse?'+queryString;
-
-  el.onerror = function onFrameError(e){
-    if (!(el.contentDocument || el.contentWindow.document || '').title.match('Thanks')){
-      console.log('An error happened submitting the response');
-    }
-  };
-
-  el.onload = function onSuccess(e){
-    w.location = m.getMailtoUrl();
-  };
-
-  d.body.appendChild(el);
 };
 
 restoreFields(fields);
